1844c5b8c36631f21216fbe8f223d2524a2efc16
Brent Buchholz <brentbuchholz@gmail.com>
2025-02-12 19:57:28 -0600

    Add slips

--- src/components/salvageSlips.js
+++ src/components/salvageSlips.js
@@ -1,3 +1,70 @@
+const slips = [
+  {
+    siteName: "sylvan-circle",
+    siteSlips: [
+      "turn image into card",
+      "delete site aftwards"
+    ]
+  },
+  {
+    siteName: "jacobs-salvage",
+    siteSlips: [
+      "site can be deleted from netlify",
+      
+    ]
+  },
+  {
+    siteName: "baolg",
+    siteSlips: [
+      "delete site",
+    ]
+  },
+  {
+    siteName: "thatb",
+    siteSlips: [
+      "delete",
+    ]
+  },
+  {
+    siteName: "thasg",
+    siteSlips: [
+      "remix these cards",
+      "delete site when finished"
+    ]
+  },
+  {
+    siteName: "tha-stratagem",
+    siteSlips: [
+      "remix these cards",
+      "delete site when finished"
+    ]
+  },
+  {
+    siteName: "cgba",
+    siteSlips: [
+      "delete",
+    ]
+  },
+  {
+    siteName: "mccu",
+    siteSlips: [
+      "delete",
+    ]
+  },
+  {
+    siteName: "sonic-circle",
+    siteSlips: [
+      "remix image into card",
+      'delete'
+    ]
+  }
+]
+
+export function getSiteNames(){
+  return slips.map(site => site.siteName);
+}
+
+
 export function getSlipsForSiteName(siteName){
-  return ["test1", "test2"]
+  return slips.find(site => site.siteName === siteName).siteSlips;
 }
